e23f56fb21dfb0d263386f1f9c4bfdfbd5da3db4
prasannachinnapareddy <59555038+prasannachinnapareddy@users.noreply.github.com>
2020-03-10 12:01:09 +0530

    Update DateTime.java

--- DateTime.java
+++ DateTime.java
@@ -1,12 +1,8 @@
-import java.time.LocalDate;
-import java.time.LocalTime;
 public class DateTime
 {
 	public static void main(String[] args)
 	{
-		LocalDate date=LocalDate.now();
-		LocalTime time=LocalTime.now();
-		System.out.println("Today date is :",+date);
-		System.out.println("Today time is :",+time);
+		System.out.println("Hii");
+		System.out.println("Hello);
 	}
 }
